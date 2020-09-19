6c1914d09953bfab77cedbf1c2c0df11dfc80efa
timlathy <tim.labushev@gmail.com>
2020-09-19 22:28:09 +0300

    [st lab1] test names should reflect assertions

--- Software-Testing-7th-Term/Lab1/src/test/kotlin/itmo/se/lab1/TrigApproxTest.kt
+++ Software-Testing-7th-Term/Lab1/src/test/kotlin/itmo/se/lab1/TrigApproxTest.kt
@@ -5,20 +5,20 @@ import org.junit.jupiter.api.Assertions
 
 class TrigApproxTest {
     @Test
-    fun `test sin(x) at inflection points`() {
+    fun `sine matches expected values at inflection points`() {
         Assertions.assertEquals(0.0, TrigApprox.sin(-Math.PI), 1E-6, "sin(-pi) = 0")
         Assertions.assertEquals(0.0, TrigApprox.sin(0.0), 1E-6, "sin(0) = 0")
         Assertions.assertEquals(0.0, TrigApprox.sin(Math.PI), 1E-6, "sin(pi) = 0")
     }
 
     @Test
-    fun `test sin(x) at extremum points`() {
+    fun `sine matches expected values at extremum points`() {
         Assertions.assertEquals(1.0, TrigApprox.sin(Math.PI / 2), 1E-6, "sin(pi/2) = 1")
         Assertions.assertEquals(-1.0, TrigApprox.sin(-Math.PI / 2), 1E-6, "sin(-pi/2) = -1")
     }
 
     @Test
-    fun `test sin(x) periodicity`() {
+    fun `sine is periodic`() {
         for (i in 0..4) {
             Assertions.assertEquals(0.5, TrigApprox.sin(2 * i * Math.PI + Math.PI / 6), 1E-6, "sin(${i * 2}pi + pi/6) = 0.5")
             Assertions.assertEquals(-0.5, TrigApprox.sin(-2 * i * Math.PI - Math.PI / 6), 1E-6, "sin(-${i * 2}pi - pi/6) = -0.5")
@@ -26,25 +26,25 @@ class TrigApproxTest {
     }
 
     @Test
-    fun `test sine of infinity`() {
+    fun `sine of infinity is nan`() {
         Assertions.assertEquals(Double.NaN, TrigApprox.sin(Double.POSITIVE_INFINITY),"sin(+inf) = NaN")
         Assertions.assertEquals(Double.NaN, TrigApprox.sin(Double.NEGATIVE_INFINITY),"sin(-inf) = NaN")
     }
 
     @Test
-    fun `test sine of NaN`() {
+    fun `sine of nan is nan`() {
         Assertions.assertEquals(Double.NaN, TrigApprox.sin(Double.NaN, eps = 1E-6),"sin(NaN) = NaN")
     }
 
     @Test
-    fun `test sine precision`() {
+    fun `sine approximation meets requested precision`() {
         Assertions.assertEquals(0.5, TrigApprox.sin(Math.PI / 6, eps = 1.0), 1.0, "sin(pi/6) = 0.5, eps = 1.0")
         Assertions.assertEquals(0.5, TrigApprox.sin(Math.PI / 6, eps = 1E-8), 1E-8, "sin(pi/6) = 0.5, eps = 1e-8")
         Assertions.assertEquals(0.5, TrigApprox.sin(Math.PI / 6, eps = 1E-16), 1E-16, "sin(pi/6) = 0.5, eps = 1e-16")
     }
 
     @Test
-    fun `test sine invalid precision`() {
+    fun `sine with invalid precision is nan`() {
         Assertions.assertEquals(Double.NaN, TrigApprox.sin(Math.PI / 6, eps = Double.POSITIVE_INFINITY), "sin(x) = NaN, eps = +inf")
         Assertions.assertEquals(Double.NaN, TrigApprox.sin(Math.PI / 6, eps = Double.NEGATIVE_INFINITY), "sin(x) = NaN, eps = -inf")
         Assertions.assertEquals(Double.NaN, TrigApprox.sin(Math.PI / 6, eps = Double.NaN), "sin(x) = NaN, eps = NaN")
