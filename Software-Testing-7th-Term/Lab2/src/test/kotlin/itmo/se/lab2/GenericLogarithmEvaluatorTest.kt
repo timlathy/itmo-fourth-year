46b7997c1f094e8bd9556b21fd03fbe8bbe9c92d
timlathy <tim.labushev@gmail.com>
2020-10-02 19:55:45 +0300

    [st lab2] finish generic log eval tests

--- Software-Testing-7th-Term/Lab2/src/test/kotlin/itmo/se/lab2/GenericLogarithmEvaluatorTest.kt
+++ Software-Testing-7th-Term/Lab2/src/test/kotlin/itmo/se/lab2/GenericLogarithmEvaluatorTest.kt
@@ -1,28 +1,84 @@
 package itmo.se.lab2
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
 import org.mockito.ArgumentMatchers.*
 import org.mockito.Mockito
 
 class GenericLogarithmEvaluatorTest {
-    @ParameterizedTest
-    @CsvFileSource(resources = ["/log.csv"], numLinesToSkip = 1)
-    fun `it computes logarithm of any base with mock ln evaluator`(y: Double, x: Double, base: Double, logX: Double, logBase: Double, eps: Double) {
-        val mockLnEval = Mockito.mock(ILogarithmEvaluator::class.java)
-        Mockito.`when`(mockLnEval.ln(eq(x), anyDouble())).thenReturn(logX)
-        Mockito.`when`(mockLnEval.ln(eq(base), anyDouble())).thenReturn(logBase)
+    @Nested
+    inner class GenericLogarithmEvaluatorUnitTest {
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/generic-log.csv"], numLinesToSkip = 1)
+        fun `it computes logarithm of any base`(
+            y: Double,
+            x: Double,
+            base: Double,
+            logX: Double,
+            logBase: Double,
+            eps: Double
+        ) {
+            val mockLnEval = Mockito.mock(ILogarithmEvaluator::class.java)
+            Mockito.`when`(mockLnEval.ln(eq(x), anyDouble())).thenReturn(logX)
+            Mockito.`when`(mockLnEval.ln(eq(base), anyDouble())).thenReturn(logBase)
 
-        val logEval = GenericLogarithmEvaluator(mockLnEval)
-        Assertions.assertEquals(y, logEval.log(x, base, eps), eps)
+            val logEval = GenericLogarithmEvaluator(mockLnEval)
+            Assertions.assertEquals(y, logEval.log(x, base, eps), eps)
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/generic-log-invalid.csv"], numLinesToSkip = 1)
+        fun `it returns NaN when called with invalid parameters`(
+            y: Double,
+            x: Double,
+            base: Double,
+            logX: Double,
+            logBase: Double,
+            eps: Double
+        ) {
+            val mockLnEval = Mockito.mock(ILogarithmEvaluator::class.java)
+            Mockito.`when`(mockLnEval.ln(eq(x), doubleThat { !(it.isNaN() || it.isInfinite()) })).thenReturn(logX)
+            Mockito.`when`(mockLnEval.ln(eq(base), doubleThat { !(it.isNaN() || it.isInfinite()) })).thenReturn(logBase)
+            Mockito.`when`(mockLnEval.ln(eq(x), doubleThat { it.isNaN() || it.isInfinite() })).thenReturn(Double.NaN)
+            Mockito.`when`(mockLnEval.ln(eq(base), doubleThat { it.isNaN() || it.isInfinite() })).thenReturn(Double.NaN)
+
+            val logEval = GenericLogarithmEvaluator(mockLnEval)
+            Assertions.assertEquals(y, logEval.log(x, base, eps), 10E-6)
+        }
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = ["/log.csv"], numLinesToSkip = 1)
-    fun `it computes logarithm of any base with real ln evaluator`(y: Double, x: Double, base: Double, _logX: Double, _logBase: Double, eps: Double) {
-        val lnEval = LogarithmEvaluator()
-        val logEval = GenericLogarithmEvaluator(lnEval)
-        Assertions.assertEquals(y, logEval.log(x, base, eps), eps)
+    @Nested
+    inner class GenericLogarithmEvaluatorIntegrationTest {
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/generic-log.csv"], numLinesToSkip = 1)
+        fun `it computes logarithm of any base`(
+            y: Double,
+            x: Double,
+            base: Double,
+            _logX: Double,
+            _logBase: Double,
+            eps: Double
+        ) {
+            val lnEval = LogarithmEvaluator()
+            val logEval = GenericLogarithmEvaluator(lnEval)
+            Assertions.assertEquals(y, logEval.log(x, base, eps), eps)
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/generic-log-invalid.csv"], numLinesToSkip = 1)
+        fun `it returns NaN when called with invalid parameters`(
+            y: Double,
+            x: Double,
+            base: Double,
+            _logX: Double,
+            _logBase: Double,
+            eps: Double
+        ) {
+            val lnEval = LogarithmEvaluator()
+            val logEval = GenericLogarithmEvaluator(lnEval)
+            Assertions.assertEquals(y, logEval.log(x, base, eps), 10E-6)
+        }
     }
 }
