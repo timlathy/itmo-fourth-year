341eeaab107540f9beb1108699c55e556a98adb4
timlathy <tim.labushev@gmail.com>
2020-10-05 21:25:22 +0300

    [st lab2] equation system x <= 0 tests

--- Software-Testing-7th-Term/Lab2/src/test/kotlin/itmo/se/lab2/EquationSystemTest.kt
+++ Software-Testing-7th-Term/Lab2/src/test/kotlin/itmo/se/lab2/EquationSystemTest.kt
@@ -1,44 +1,106 @@
 package itmo.se.lab2
 
-import itmo.se.lab2.EquationSystem
-import itmo.se.lab2.IGenericLogarithmEvaluator
-import itmo.se.lab2.IGenericTrigonometryEvaluator
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
-import org.mockito.ArgumentMatchers.eq
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class EquationSystemTest {
-    @ParameterizedTest
-    @CsvFileSource(resources = ["/equation-system-stub.csv"], numLinesToSkip = 1)
-    fun `it handles division by zero`(
+    fun mockTrigEvaluator(
         x: Double,
-        y: Double,
         sin: Double,
         tan: Double,
         cot: Double,
         sec: Double,
+        eps: Double
+    ): IGenericTrigonometryEvaluator =
+        mock(IGenericTrigonometryEvaluator::class.java).apply {
+            `when`(sin(eq(x), eq(eps))).thenReturn(sin)
+            `when`(tan(eq(x), eq(eps))).thenReturn(tan)
+            `when`(cot(eq(x), eq(eps))).thenReturn(cot)
+            `when`(sec(eq(x), eq(eps))).thenReturn(sec)
+        }
+
+    fun mockLogEvaluator(
+        x: Double,
         log2: Double,
         log3: Double,
         log5: Double,
         log10: Double,
         eps: Double
-    ) {
-        val mockTrig = mock(IGenericTrigonometryEvaluator::class.java)
-        `when`(mockTrig.sin(eq(x), eq(eps))).thenReturn(sin)
-        `when`(mockTrig.tan(eq(x), eq(eps))).thenReturn(tan)
-        `when`(mockTrig.cot(eq(x), eq(eps))).thenReturn(cot)
-        `when`(mockTrig.sec(eq(x), eq(eps))).thenReturn(sec)
+    ): IGenericLogarithmEvaluator =
+        mock(IGenericLogarithmEvaluator::class.java).apply {
+            `when`(log(eq(x), eq(2.0), eq(eps))).thenReturn(log2)
+            `when`(log(eq(x), eq(3.0), eq(eps))).thenReturn(log3)
+            `when`(log(eq(x), eq(5.0), eq(eps))).thenReturn(log5)
+            `when`(log(eq(x), eq(10.0), eq(eps))).thenReturn(log10)
+        }
 
-        val mockLog = mock(IGenericLogarithmEvaluator::class.java)
-        `when`(mockLog.log(eq(x), eq(2.0), eq(eps))).thenReturn(log2)
-        `when`(mockLog.log(eq(x), eq(3.0), eq(eps))).thenReturn(log3)
-        `when`(mockLog.log(eq(x), eq(5.0), eq(eps))).thenReturn(log5)
-        `when`(mockLog.log(eq(x), eq(10.0), eq(eps))).thenReturn(log10)
+    @Nested
+    inner class EquationSystemUnitTest {
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/equation-system.csv"], numLinesToSkip = 1)
+        fun `it computes the equation for valid inputs`(
+            x: Double,
+            y: Double,
+            sin: Double,
+            tan: Double,
+            cot: Double,
+            sec: Double,
+            log2: Double,
+            log3: Double,
+            log5: Double,
+            log10: Double,
+            eps: Double
+        ) {
+            val mockTrig = mockTrigEvaluator(x, sin, tan, cot, sec, eps)
+            val mockLog = mockLogEvaluator(x, log2, log3, log5, log10, eps)
+            val system = EquationSystem(mockTrig, mockLog)
+            Assertions.assertEquals(y, system.compute(x, eps), eps)
+        }
+    }
 
-        val system = EquationSystem(mockTrig, mockLog)
-        Assertions.assertEquals(y, system.compute(x, eps), eps)
+    @Nested
+    inner class EquationSystemIntegrationTest {
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/equation-system.csv"], numLinesToSkip = 1)
+        fun `it integrates logarithm evaluator`(
+            x: Double,
+            y: Double,
+            sin: Double,
+            tan: Double,
+            cot: Double,
+            sec: Double,
+            _log2: Double,
+            _log3: Double,
+            _log5: Double,
+            _log10: Double,
+            eps: Double
+        ) {
+            val mockTrig = mockTrigEvaluator(x, sin, tan, cot, sec, eps)
+            val system = EquationSystem(mockTrig, GenericLogarithmEvaluator(LogarithmEvaluator()))
+            Assertions.assertEquals(y, system.compute(x, eps), eps)
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = ["/equation-system.csv"], numLinesToSkip = 1)
+        fun `it integrates trigonometry evaluator`(
+            x: Double,
+            y: Double,
+            _sin: Double,
+            _tan: Double,
+            _cot: Double,
+            _sec: Double,
+            log2: Double,
+            log3: Double,
+            log5: Double,
+            log10: Double,
+            eps: Double
+        ) {
+            val mockLog = mockLogEvaluator(x, log2, log3, log5, log10, eps)
+            val system = EquationSystem(GenericTrigonometryEvaluator(TrigonometryEvaluator()), mockLog)
+            Assertions.assertEquals(y, system.compute(x, eps), eps)
+        }
     }
 }
